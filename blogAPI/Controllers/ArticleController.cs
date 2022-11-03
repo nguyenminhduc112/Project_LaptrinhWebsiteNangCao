9a9603e648ae00021c5b2c94334ea6cc4dc1aa7d
nguyenminhduc112 <nguyenminhduc2001pt@gmail.com>
2022-11-03 16:26:20 +0700

    complete article

--- blogAPI/Controllers/ArticleController.cs
+++ blogAPI/Controllers/ArticleController.cs
@@ -1,5 +1,7 @@
 using blogAPI.Data;
+using blogAPI.Dto.Article;
 using blogAPI.Dto.User;
+using blogAPI.Models;
 using blogAPI.Responsitories;
 using Microsoft.AspNetCore.Mvc;
 namespace blogAPI.Controllers
@@ -38,6 +40,31 @@ namespace blogAPI.Controllers
             }else{
                 return BadRequest(ModelState.ErrorCount);
             }
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> editArticleAsync(Guid Id, PutArticleDto putArticleDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var userNew = new Article()
+                {
+                    Title = putArticleDto.Title,
+                    Content = putArticleDto.Content,
+                };
+                return Ok(await _context.EditArticle(Id, userNew));
+
+            }
+            else
+            {
+                return BadRequest(ModelState.ErrorCount);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteArticle([FromQuery] Guid id)
+        {
+            return Ok(await _context.DeleteArticle(id));
         }
     }
 }
---
+++ blogAPI/Dto/Article/PutArticleDto.cs
@@ -0,0 +1,8 @@
+namespace blogAPI.Dto.Article
+{
+    public class PutArticleDto
+    {
+        public String Title { get; set; }
+        public String Content { get; set; }
+    }
+}
